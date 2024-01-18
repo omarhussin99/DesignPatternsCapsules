bc429ae374751df3dcb2e54eb97779b4940b7214
Omar <omar_hussin99@yahoo.com>
2024-01-18 21:26:28 +0300

    Add ChessPiece docs

--- src/main/kotlin/creational/factorymethod/ChessPiece.kt
+++ src/main/kotlin/creational/factorymethod/ChessPiece.kt
@@ -1,5 +1,11 @@
 package creational.factorymethod
 
+/**
+ * Represents a chess piece on the chessboard.
+ *
+ * @property file The file (column) of the chessboard where the piece is located.
+ * @property rank The rank (row) of the chessboard where the piece is located.
+ */
 interface ChessPiece {
     val file: Char
     val rank: Char
