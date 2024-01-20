e16532f206812e2998aafae249ffc0ff5bf3c0e9
Omar <omar_hussin99@yahoo.com>
2024-01-20 19:44:00 +0300

    Add createPiece function

--- src/main/kotlin/creational/factorymethod/Main.kt
+++ src/main/kotlin/creational/factorymethod/Main.kt
@@ -1,7 +1,24 @@
 package creational.factorymethod
 
-fun main(args: Array<String>) {
-    println("Hello World!")
-    println("Program arguments: ${args.joinToString()}")
+import java.lang.RuntimeException
+
+fun main() {
+    createPiece("qa3")
+}
+
+/**
+ * Creates a chess piece based on the given notation.
+ *
+ * @param notation The notation representing the chess piece.
+ * @return A chess piece object based on the provided notation.
+ * @throws RuntimeException if the notation represents an unknown piece.
+ */
+fun createPiece(notation : String) : ChessPiece {
+    val (type,file,rank) = notation.toCharArray()
+    return when (type) {
+        'q' -> Queen(file,rank)
+        'p' -> Pawn(file, rank)
+        else -> throw RuntimeException("Unknown piece: $type")
+    }
 }
 
